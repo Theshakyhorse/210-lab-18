013d7cc1bc4cf1e7c926d072d6acb58f035c8cb7
Theshakyhorse <amaximus.sierra@gmail.com>
2026-03-08 17:51:36 -0700

    second

--- main.cpp
+++ main.cpp
@@ -2,7 +2,48 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+struct Node {
+    float rating;
+    string comment;
+    Node *next;
+};
 
+void frontN(Node *&, Node *);
+void tailN(Node *&, Node *);
+void output(Node *);
+
+int main() {
+    int choice = 0;
+    cout << "[1] new nodes are added to the head of linked list" << endl;
+    cout << "[2] new nodes are added to the tail of linked list" << endl;
+    cin >> choice;
     return 0;
+}
+
+void frontN(Node *&head, Node *newnode) {
+    if(!head) {
+        head = newnode;
+        newnode->next=nullptr;
+    }
+    else {
+        newnode->next=head;
+        head = newnode;
+    }
+}
+
+void tailN(Node *&head, Node *newnode) {
+    if(!head) {
+        head = newnode;
+        newnode->next=nullptr;
+    }
+    else{
+        Node *current=head;
+        while (current) {
+            if ((current->next)==nullptr) {
+                current->next=newnode;
+                newnode->next=nullptr;
+            }
+            current = current->next;
+        }
+    }
 }
