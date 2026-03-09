9a50cea876690a90b30b69f8f0ef23e2dd19167f
Theshakyhorse <amaximus.sierra@gmail.com>
2026-03-08 18:33:19 -0700

    sixth

--- main.cpp
+++ main.cpp
@@ -11,11 +11,14 @@ struct Node {
 void frontN(Node *&, Node *);
 void tailN(Node *&, Node *);
 void output(Node *);
+void deleteL(Node *&);
 
 int main() {
+    //declarations
     Node *head = nullptr;
     int choice = 0;
 
+    //giving choice of adding to head or tail
     cout << "[1] New nodes are added to the head of linked list" << endl;
     cout << "[2] New nodes are added to the tail of linked list" << endl;
     cin >> choice;
@@ -24,8 +27,9 @@ int main() {
         cin >> choice;
     }
 
+    //code for head
+    Node *newnode = nullptr;
     while (choice == 1) {
-        Node *newnode = nullptr;
         cout << "Enter review rating 0-5:" << endl;
         cin >> newnode->rating;
         cout << "Enter review comment:" << endl;
@@ -37,10 +41,11 @@ int main() {
             cout << "Please select a valid option of 1 or 0" << endl;
             cin >> choice;
         }
+        deleteL(newnode);
     }
 
+    //code for tail
     while (choice == 2) {
-        Node *newnode = nullptr;
         cout << "Enter review rating 0.0-5.0:" << endl;
         cin >> newnode->rating;
         cout << "Enter review comment:" << endl;
@@ -52,14 +57,17 @@ int main() {
             cout << "Please select a valid option of 2 or 0" << endl;
             cin >> choice;
         }
+        deleteL(newnode);
     }
 
     cout << "Outputting all reviews: " << endl;
     output(head);
+    deleteL(head);
 
     return 0;
 }
 
+//adds node to head
 void frontN(Node *&head, Node *newnode) {
     if(!head) {
         head = newnode;
@@ -71,6 +79,7 @@ void frontN(Node *&head, Node *newnode) {
     }
 }
 
+//adds node to tail
 void tailN(Node *&head, Node *newnode) {
     if(!head) {
         head = newnode;
@@ -88,6 +97,7 @@ void tailN(Node *&head, Node *newnode) {
     }
 }
 
+//outputs linked list
 void output(Node *hd) {
     if (!hd) {
         cout << "empty list.\n";
@@ -95,10 +105,23 @@ void output(Node *hd) {
     }
 
     int count = 1;
+    double sum = 0;
     Node *current = hd;
     while (current) {
-        cout << "Review #" << count << ": ";
-        cout << current->rating << ": " << current ->comment;
+        cout << " > Review #" << count++ << ": ";
+        cout << current->rating << ": " << current ->comment << endl;
+        sum += current->rating;
     }
-    cout << endl;
+    cout << " > Average: " << sum/count << endl;
+}
+
+//deletes a linked list
+void deleteL(Node *&n) {
+    Node *current = n;
+    while (current) {
+        n = current ->next;
+        delete current;
+        current = n;
+    }
+    n = nullptr;
 }
